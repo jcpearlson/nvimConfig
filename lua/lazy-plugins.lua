965ef65435f64561d0c80d36f52fd70680e30dfe
jcpearlson <jcpearlson@gmail.com>
2025-01-05 17:12:21 -0500

    new packages

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -47,6 +47,19 @@ require("lazy").setup({
 	require("custom/plugins/nvim-tree"),
 
 	require("custom/plugins/alpha"),
+
+	require("custom/plugins/auto-session"),
+
+	require("custom/plugins/bufferline"),
+
+	require("custom/plugins/lualine"),
+
+	require("custom/plugins/dressing"),
+
+	require("custom/plugins/vim-maximizer"),
+
+	require("custom/plugins/init"),
+
 	-- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
 	-- init.lua. If you want these files, they are in the repository, so you can just download them and
 	-- place them in the correct locations.
